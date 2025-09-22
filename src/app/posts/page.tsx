ddfa7a15314dd2686c181c8df328bd13852ca0da
ZorittoWendy <zorittoprogramador@gmail.com>
2025-09-22 18:16:04 -0300

    feat: Rotas dinamicas

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,36 +1,67 @@
 // SERVER COMPONENTS
 
-import { Button } from "@/components/button";
+import Link from "next/link";
 
-interface PostProps{
-    id: number,
-    title: string,
-    body: string,
-    userId: number
+export interface PostProps {
+  id: number;
+  title: string;
+  body: string;
+  userId: number;
 }
 
-interface ResponseProps{
-    posts: PostProps[]
+interface ResponseProps {
+  posts: PostProps[];
 }
 
-export default async function PostsPage(){
-    const response = await fetch('https://dummyjson.com/posts')
-    const data: ResponseProps = await response.json()
+export default async function PostsPage() {
+  const response = await fetch("https://dummyjson.com/posts");
+  const data: ResponseProps = await response.json();
+
+  async function handleFetchPosts() {
+    "use server";
+    const response = await fetch("https://dummyjson.com/posts");
+    const data: ResponseProps = await response.json();
+
+    console.log(data.posts);
+  }
+
+  async function handleSeacherUsers(formData: FormData) {
+    "use server";
+    const userId = formData.get("userId");
+
+    const response = await fetch(`https://dummyjson.com/posts/user/${userId}`);
+    const data: ResponseProps = await response.json();
 
     console.log(data);
-    return(
-        <div>
-            <h1 className="text-center mt-5 mb-2 font-bold text-3xl">Todos os posts</h1>
+  }
 
-            <Button/>
-            <div className="flex flex-col gap-4 mx-2">
-                {data.posts.map(post =>(
-                    <div key={post.id} className="bg-gray-200 p-4 rounded-md">
-                        <h2 className="font-bold">{post.title}</h2>
-                        <p>{post.body}</p>
-                    </div>
-                ))}
-            </div>
-        </div>
-    )
+  return (
+    <div>
+      <h1 className="text-center mt-5 mb-2 font-bold text-3xl">
+        Todos os posts
+      </h1>
+
+      <button onClick={handleFetchPosts}>Buscar posts</button>
+
+      <form className="flex gap-2 my-2" action={handleSeacherUsers}>
+        <input
+          type="text"
+          placeholder="ID do úsuario"
+          className="border border-gray-200 p-2"
+          name="userId"
+        />
+      </form>
+      <div className="flex flex-col gap-4 mx-2">
+        {data.posts.map((post) => (
+          <div key={post.id} className="bg-gray-200 p-4 rounded-md">
+            <h2 className="font-bold">{post.title}</h2>
+            <p>{post.body}</p>
+            <Link className="text-blue-500" href={`/posts/${post.id}`}>
+              Acessar detalhes
+            </Link>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
