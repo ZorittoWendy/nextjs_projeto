f0cd58f7803a0a8c13d01b9129351c52054fa119
ZorittoWendy <zorittoprogramador@gmail.com>
2025-09-24 15:15:14 -0300

    feat: add revalidade, cache e ISR

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,31 @@
+import { Metadata } from "next";
 
-export default function Home (){
+export const metadata: Metadata = {
+  title: "Home - Aula Next JS do zero",
+  description: "Aprendendo Next JS do zero com Sujeito programador",
+  openGraph: {
+    title: "Aprendendo Next no youtube",
+    description: "Aprendendo Nextjs do zero com o Sujeito programador",
+  },
+  robots: {
+    index: true,
+    follow: true,
+    nocache: true,
+    googleBot: {
+      index: true,
+      follow: true,
+    },
+  },
+};
+
+export const revalidate = 60;
+
+export default function Home() {
+  const randomNumber = Math.random() * 10;
   return (
     <div>
       <h1>Page Home</h1>
+      <h2>Numero gerado: {randomNumber}</h2>
     </div>
-  )
+  );
 }
